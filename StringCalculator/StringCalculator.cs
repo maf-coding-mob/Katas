7f53d1dcb2ffcfd4e39253530edadfa780e36bef
Pham Kim Toàn <kim-toan.pham@maf.fr>
2016-08-12 14:34:50 +0200

    ajout de kata StringCalculator

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == String.Empty)
+            {
+                return 0;
+            }
+            string[] digits = GetDigits(numbers);
+            var result = SumDigits(digits);
+            return result;
+        }
+
+        private string[] GetDigits(string numbers)
+        {
+            if (Regex.IsMatch(numbers, "^//.{1}\n.*?"))
+            {
+                var delimiter = Regex.Match(numbers, "^//(?<DELIMITER>.{1})\n.*?").Groups["DELIMITER"].Value;
+                numbers = numbers.Split(new[] { "\n" }, StringSplitOptions.None)[1];
+                return numbers.Split(new[] { delimiter }, StringSplitOptions.None);
+            }
+            return numbers.Split(new[] { ",", "\n" }, StringSplitOptions.None);
+        }
+        private int SumDigits(string[] digits)
+        {
+            return digits.Sum(item => Int32.Parse(item));
+        }
+
+    }
+}
---
+++ StringCalculator/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+
+namespace StringCalculator
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void Returns_Expected_Sum_When_Multiple_Numbers()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("1,2");
+            Assert.Equal(3, actual);
+        }
+
+
+        [Fact]
+        public void Returns_0_When_Empty()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add(string.Empty);
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Returns_0_When_0()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("0");
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Returns_Expected_Sum_When_Multiple_Numbers_With_CRLF()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("1\n2");
+            Assert.Equal(3, actual);
+        }
+
+
+        [Fact]
+        public void Returns_Expected_Sum_When_Multiple_Numbers_With_CR()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("1\n2,3");
+            Assert.Equal(6, actual);
+        }
+
+        [Fact]
+        public void Returns_3_When_Use_Custom_Delimiter()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("//;\n1;2");
+            Assert.Equal(3, actual);
+        }
+
+    }
+
+}
+
