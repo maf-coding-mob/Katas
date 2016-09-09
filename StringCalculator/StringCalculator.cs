378fb536ad978bbac20326215fd7897ff64a498f
Aubin Alois <alois.aubin@maf.fr>
2016-09-09 14:07:26 +0200

    StringCalculator

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,39 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace StringCalculator
 {
     public class StringCalculator
     {
-        public int Add(string numbers)
+        internal int Add(string v)
         {
-            if (numbers == String.Empty)
-            {
-                return 0;
-            }
-            string[] digits = GetDigits(numbers);
-            var result = SumDigits(digits);
-            return result;
+            v>ingSplitOptions()
+            if (v == "") return 0;
+            return int.Parse(v);
         }
-
-        private string[] GetDigits(string numbers)
-        {
-            if (Regex.IsMatch(numbers, "^//.{1}\n.*?"))
-            {
-                var delimiter = Regex.Match(numbers, "^//(?<DELIMITER>.{1})\n.*?").Groups["DELIMITER"].Value;
-                numbers = numbers.Split(new[] { "\n" }, StringSplitOptions.None)[1];
-                return numbers.Split(new[] { delimiter }, StringSplitOptions.None);
-            }
-            return numbers.Split(new[] { ",", "\n" }, StringSplitOptions.None);
-        }
-        private int SumDigits(string[] digits)
-        {
-            return digits.Sum(item => Int32.Parse(item));
-        }
-
     }
 }
---
+++ StringCalculator/StringCalculatorShould.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace StringCalculator
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void Add_With_Empty_Return_0()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("");
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Return_1_When_1()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("1");
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void Return_9_When_9()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("9");
+            Assert.Equal(9, actual);
+        }
+
+        [Fact]
+        public void Return_3_When_1_And2()
+        {
+            var stringCalculator = new StringCalculator();
+            var actual = stringCalculator.Add("1,2");
+            Assert.Equal(3, actual);
+        }
+    }
+}
+
